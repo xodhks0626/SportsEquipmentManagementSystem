455a2688b4a75175531244a87887001f3fb028b8
Taewan <Taewan@192.168.219.106>
2020-03-19 17:35:44 +0900

    Create a menu

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package hellowworld;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		System.out.println("I am a new Java porgrammer!");
-	}
-}
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("----SportsEquipment Management System Menu----");
+		System.out.println("1. Add SportsEquipment");
+		System.out.println("2. Delete SportsEquipment");
+		System.out.println("3. Edit SportsEquipment");
+		System.out.println("4. View SportsEquipment");
+		System.out.println("5. Exit");
+		System.out.println("1. Select one number between 1 - 5 :");
+	}
+}
