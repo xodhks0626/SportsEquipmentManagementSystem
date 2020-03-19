4524d88338578414d3ac97db3bd0a0579c43cb87
xodhks0626 <62197677+xodhks0626@users.noreply.github.com>
2020-03-19 17:17:37 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,8 @@
-hello
+package hellowworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.println("I am a new Java porgrammer!");
+	}
+}
